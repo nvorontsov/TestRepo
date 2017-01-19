bb666b8062d69b0f1c052f2adeb1f69c5f4d3e16
Nikolay <n.vorontsov@gmail.com>
2017-01-18 22:36:11 -0800

    Initial Commit

---
+++ TestRepo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestRepo
+//
+//  Created by Nikolay on 1/18/17.
+//  Copyright © 2017 Nikolay Vorontsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
